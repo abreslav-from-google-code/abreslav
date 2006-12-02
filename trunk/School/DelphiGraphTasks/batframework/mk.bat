a74d9213f304ac4af85959015ab0f047e4b1f912
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2006-12-02 06:37:56 +0000

    Initial commit

---
+++ trunk/School/DelphiGraphTasks/batframework/mk.bat
@@ -0,0 +1,32 @@
+cd tasks
+set ddd=tasks4_01
+md %ddd%
+copy *_01.exe %ddd%
+
+set ddd=tasks4_02
+md %ddd%
+copy *_02.exe %ddd%
+
+set ddd=tasks4_03
+md %ddd%
+copy *_03.exe %ddd%
+
+set ddd=tasks4_04
+md %ddd%
+copy *_04.exe %ddd%
+
+set ddd=tasks4_05
+md %ddd%
+copy *_05.exe %ddd%
+
+set ddd=tasks4_06
+md %ddd%
+copy *_06.exe %ddd%
+copy *4_05.exe %ddd%
+copy *5_01.exe %ddd%
+
+set ddd=tasks4_07
+md %ddd%
+copy *_07.exe %ddd%
+copy *4_04.exe %ddd%
+copy *5_05.exe %ddd%
