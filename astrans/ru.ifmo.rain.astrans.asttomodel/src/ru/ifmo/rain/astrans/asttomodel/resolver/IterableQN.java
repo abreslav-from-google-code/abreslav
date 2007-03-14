c7f7eb86fbb5af54ec71fa1788ffae7566924f2c
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2007-03-14 18:15:58 +0000

    IterableQN created EPackageResolver created Resolver taken out from Transformation class Tests created

---
+++ astrans/ru.ifmo.rain.astrans.asttomodel/src/ru/ifmo/rain/astrans/asttomodel/resolver/IterableQN.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package ru.ifmo.rain.astrans.asttomodel.resolver;
+
+import java.util.Iterator;
+
+import astransast.QualifiedName;
+
+class IterableQN implements Iterable<String> {
+	protected class QNIterator implements Iterator<String> {
+		private QualifiedName currentQN;
+
+		public QNIterator(QualifiedName qn) {
+			currentQN = qn;
+		}
+		
+		public boolean hasNext() {
+			return currentQN != null;
+		}
+
+		public String next() {
+			String result = currentQN.getName();
+			currentQN = currentQN.getSubQN();						
+			return result;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		protected QualifiedName getCurrentQN() {
+			return currentQN;
+		}
+	}
+
+	private final QualifiedName qn;
+	
+	public IterableQN(final QualifiedName qn) {
+		this.qn = qn;
+	}
+
+	public Iterator<String> iterator() {
+		return new QNIterator(qn);
+	}
+	
+	protected QualifiedName getQN() {
+		return qn;
+	}
+}
