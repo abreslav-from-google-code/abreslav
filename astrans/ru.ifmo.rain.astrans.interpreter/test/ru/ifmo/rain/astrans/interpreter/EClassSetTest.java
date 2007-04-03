60b795c019db592bba843a6eed560ad764a913bc
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2007-04-03 19:39:32 +0000

    TranslateReferences tested Skipper logic reused in ReferenceTranslator

--- astrans/ru.ifmo.rain.astrans.interpreter/test/ru/ifmo/rain/astrans/interpreter/EClassSetTest.java
+++ astrans/ru.ifmo.rain.astrans.interpreter/test/ru/ifmo/rain/astrans/interpreter/EClassSetTest.java
@@ -7,46 +7,46 @@ import org.eclipse.emf.ecore.EcorePackage;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SkipperTest {
+public class EClassSetTest {
 
-	private Skipper skipper;
+	private EClassSet skipper;
 
 	@Before
 	public void setUp() throws Exception {
-		skipper = new Skipper();
-		skipper.addSkippedEClass(EcorePackage.eINSTANCE.getEAnnotation(), false);
-		skipper.addSkippedEClass(EcorePackage.eINSTANCE.getEClassifier(), true);
+		skipper = new EClassSet();
+		skipper.addEClass(EcorePackage.eINSTANCE.getEAnnotation(), false);
+		skipper.addEClass(EcorePackage.eINSTANCE.getEClassifier(), true);
 	}
 
 	@Test
 	public final void testIsSkippedSuccessNoChildren() {
-		assertTrue(skipper.isSkipped(EcorePackage.eINSTANCE.getEAnnotation()));
+		assertTrue(skipper.contains(EcorePackage.eINSTANCE.getEAnnotation()));
 	}
 
 	@Test
 	public final void testIsSkippedFailureNoChildren() {
-		assertFalse(skipper.isSkipped(EcorePackage.eINSTANCE.getEAttribute()));
+		assertFalse(skipper.contains(EcorePackage.eINSTANCE.getEAttribute()));
 	}
 
 	@Test
 	public final void testIsSkippedSuccessWithChildren() {
-		assertTrue(skipper.isSkipped(EcorePackage.eINSTANCE.getEClass()));
+		assertTrue(skipper.contains(EcorePackage.eINSTANCE.getEClass()));
 	}
 
 	@Test
 	public final void testAddSkippedEClassNoChildren() {
 		EClass eClass = EcorePackage.eINSTANCE.getEStructuralFeature();
-		assertFalse(skipper.isSkipped(eClass));
-		skipper.addSkippedEClass(eClass, false);
-		assertTrue(skipper.isSkipped(eClass));
+		assertFalse(skipper.contains(eClass));
+		skipper.addEClass(eClass, false);
+		assertTrue(skipper.contains(eClass));
 	}
 
 	@Test
 	public final void testAddSkippedEClassWithChildren() {
 		EClass eClass = EcorePackage.eINSTANCE.getEAttribute();
-		assertFalse(skipper.isSkipped(eClass));
-		skipper.addSkippedEClass(EcorePackage.eINSTANCE.getEStructuralFeature(), true);
-		assertTrue(skipper.isSkipped(eClass));
+		assertFalse(skipper.contains(eClass));
+		skipper.addEClass(EcorePackage.eINSTANCE.getEStructuralFeature(), true);
+		assertTrue(skipper.contains(eClass));
 	}
 
 }
