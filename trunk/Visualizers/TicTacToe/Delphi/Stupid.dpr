d2ad0c2919a30d7891e2c71f3c6075b2b85e9687
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2007-08-08 19:16:47 +0000

    One known bug left: OX OX this symmetrical position somehow gets assymetrical penalties

--- trunk/Visualizers/TicTacToe/Delphi/Stupid.dpr
+++ trunk/Visualizers/TicTacToe/Delphi/Stupid.dpr
@@ -7,41 +7,99 @@ uses
   TicTacToe in 'TicTacToe.pas',
   StupidThinker in 'StupidThinker.pas';
 
+const
+  CELL_SIZE = 16;
+  CROSS_COLOR = clBlue;
+  CIRCLE_COLOR = clRed;
+  topband = 20;
+
 var
   message : String;
 
 procedure DrawField;
-const
-  S = 'I am Stupid!';
 var
-  h : Integer;
+  l, t, r, b, x, y : Integer;
+  s : String;
 begin
+  FreezeScreen;
   ClrScr;
-  SetFontStyle([fsBold]);
-  SetFontColor(clBlue);
-  SetFontSize(50);
-  SetFontName('Arial');
-  TextOut((GetMaxX - TextWidth(S)) div 2, (GetMaxY - TextHeight(S)) div 2, S);
-  h := TextHeight(S);
-  SetFontSize(20);
-  TextOut((GetMaxX - TextWidth(message)) div 2, (h + GetMaxY - TextHeight(message)) div 2, message);
+//  SetFontStyle([fsBold]);
+//  SetFontColor(mColor);
+//  TextOut((GetMaxX - TextWidth(message)) div 2, (topband - TextHeight(message)) div 2, message);
+  SetPenWidth(1);
+  SetPenColor(clBlack);
+  for x := 0 to FieldWidth - 1 do begin
+    MoveTo(x * CELL_SIZE, topband);
+    LineTo(x * CELL_SIZE, topband + FieldHeight * CELL_SIZE);
+  end;
+  for y := 0 to FieldHeight - 1 do begin
+    MoveTo(0, topband + y * CELL_SIZE);
+    LineTo(FieldWidth * CELL_SIZE, topband + y * CELL_SIZE);
+  end;
+  SetPenWidth(2);
+  for x := 0 to FieldWidth - 1 do begin
+    l := x * CELL_SIZE;
+    r := l + CELL_SIZE;
+    for y := 0 to FieldHeight - 1 do begin
+      t := topband + y * CELL_SIZE;
+      b := t + CELL_SIZE;
+      case getCell(x, y) of
+        csCross : begin
+          SetPenColor(CROSS_COLOR);
+          MoveTo(l, t);
+          LineTo(r, b);
+          MoveTo(l, b);
+          LineTo(r, t);
+        end;
+        csCircle : begin
+          SetPenColor(CIRCLE_COLOR);
+          Ellipse(l, t, r, b);
+        end;
+        else begin
+          s := IntToStr(getCellPenalty(x, y));
+          TextOut(
+            l + (CELL_SIZE - TextWidth(s)) div 2,
+            t + (CELL_SIZE - TextHeight(s)) div 2,
+            s);
+        end;
+      end;
+    end;
+  end;
+  UnFreezeScreen;
 end;
 
 procedure PlayGame;
 var
   hisTurn : TTurn;
   x, y : Integer;
+  first : Boolean;
 begin
   DrawField;
 
-  Randomize;
-  x := Random(FieldWidth);
-  y := Random(FieldHeight);
-
+  first := true;
   while true do begin
-    Decide(x, y);
+    while true do begin
+      WaitForMouseEvent;
+      if MousePressed then
+        break;
+    end;
+
+    if first and (mine = csCross) then begin
+      Randomize;
+      x := Random(FieldWidth div 2) + FieldWidth div 4;
+      y := Random(FieldHeight div 2) + FieldWidth div 4;
+    end else begin
+      Decide(x, y);
+    end;
+    first := false;
     RecordAndCalculate(x, y, mine);
+    DrawField;
+
     hisTurn := makeTurn(x, y);
+
+    RecordAndCalculate(hisTurn.x, hisTurn.y, his);
+    DrawField;
+
     case hisTurn.status of
       YourTurn: message := 'Your turn...';
       YouHaveWon: begin
@@ -57,14 +115,13 @@ begin
         break;
       end;
     end;
-
-    RecordAndCalculate(hisTurn.x, hisTurn.y, his);
-    DrawField;
   end;
 end;
 
 begin
-  InitGraph(450, 150);
+  InitGraph(FieldWidth * CELL_SIZE, FieldHeight * CELL_SIZE + topband);
+  SetFontName('Arial');
+  SetFontSize(8);
 
   if Me = Cross then begin
     mine := csCross;
