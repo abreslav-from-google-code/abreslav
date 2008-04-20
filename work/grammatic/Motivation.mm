1c2f8beaac0d839cc8199f5eb1d0ddca9dab6c3f
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2008-04-20 11:11:59 +0000

    KMU-V materials

---
+++ work/grammatic/Motivation.mm
@@ -0,0 +1,149 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1201424483140" ID="Freemind_Link_1814510319" MODIFIED="1201424864265" STYLE="bubble" TEXT="Overview of Grammarware paper">
+<node CREATED="1201424607609" FOLDED="true" ID="Freemind_Link_1428749981" MODIFIED="1201516476656" POSITION="left" STYLE="bubble" TEXT="Grammars">
+<node CREATED="1201424610546" ID="Freemind_Link_573217506" MODIFIED="1201516281796" STYLE="bubble" TEXT="Context-free">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1201424614953" ID="Freemind_Link_1944956844" MODIFIED="1201424616125" STYLE="bubble" TEXT="XML"/>
+<node CREATED="1201424616671" ID="Freemind_Link_581978229" MODIFIED="1201424620765" STYLE="bubble" TEXT="Class dictionaries"/>
+<node CREATED="1201424621000" ID="Freemind_Link_1851619985" MODIFIED="1201424625546" STYLE="bubble" TEXT="Some tree grammars"/>
+<node CREATED="1201424625796" ID="Freemind_Link_1758578609" MODIFIED="1201424633921" STYLE="bubble" TEXT="Some graph grammars"/>
+</node>
+<node CREATED="1201424679953" ID="Freemind_Link_1660506933" MODIFIED="1201436107335" POSITION="right" STYLE="bubble" TEXT="Use cases">
+<node CREATED="1201424917250" FOLDED="true" ID="Freemind_Link_237815933" MODIFIED="1201436118834" STYLE="bubble" TEXT="Grammar-dependent software">
+<node CREATED="1201424689984" ID="Freemind_Link_1675180842" MODIFIED="1201424694453" STYLE="bubble" TEXT="Parsers"/>
+<node CREATED="1201424694703" ID="Freemind_Link_623403769" MODIFIED="1201424699171" STYLE="bubble" TEXT="Program convertors"/>
+<node CREATED="1201424699421" ID="Freemind_Link_1970201250" MODIFIED="1201424703031" STYLE="bubble" TEXT="XML processors"/>
+</node>
+<node CREATED="1201424544750" FOLDED="true" ID="Freemind_Link_1890836444" MODIFIED="1201424980203" STYLE="bubble" TEXT="General use cases">
+<node CREATED="1201424572156" ID="Freemind_Link_1378428152" MODIFIED="1201424578421" STYLE="bubble" TEXT="Concrete syntax"/>
+<node CREATED="1201424578640" ID="Freemind_Link_1838147107" MODIFIED="1201424581718" STYLE="bubble" TEXT="Abstract syntax"/>
+<node CREATED="1201424582312" ID="Freemind_Link_1084928519" MODIFIED="1201424604140" STYLE="bubble" TEXT="Data exchange formats"/>
+</node>
+<node CREATED="1201424981421" FOLDED="true" ID="Freemind_Link_1970655265" MODIFIED="1201424990734" STYLE="bubble" TEXT="Technical use cases">
+<node CREATED="1201424993171" FOLDED="true" ID="Freemind_Link_590605742" MODIFIED="1201425002968" STYLE="bubble" TEXT="Import of user profiles">
+<node CREATED="1201425003687" ID="Freemind_Link_693449423" MODIFIED="1201425006765" STYLE="bubble" TEXT="Older version"/>
+<node CREATED="1201425006953" ID="Freemind_Link_258101842" MODIFIED="1201425014296" STYLE="bubble" TEXT="Competitor program"/>
+<node CREATED="1201425014515" ID="Freemind_Link_726756116" MODIFIED="1201425019421" STYLE="bubble" TEXT="Open format"/>
+</node>
+<node CREATED="1201425021375" FOLDED="true" ID="Freemind_Link_1749713433" MODIFIED="1201425123437" STYLE="bubble" TEXT="Converting DB screen definitions">
+<node CREATED="1201425124265" ID="Freemind_Link_1666897472" MODIFIED="1201425140265" STYLE="bubble" TEXT="A new definition langage"/>
+<node CREATED="1201425140484" ID="Freemind_Link_582945002" MODIFIED="1201425149796" STYLE="bubble" TEXT="From hard-coded to declarative"/>
+</node>
+<node CREATED="1201425225218" ID="Freemind_Link_303441619" MODIFIED="1201425257015" STYLE="bubble" TEXT="Convert DOM-based code to strictly typed XML bindings"/>
+<node CREATED="1201425309296" FOLDED="true" ID="Freemind_Link_1046900619" MODIFIED="1201425341750" STYLE="bubble" TEXT="Recognizing code smells">
+<node CREATED="1201425315843" ID="Freemind_Link_1306275329" MODIFIED="1201425346828" STYLE="bubble" TEXT="Errorneous API usages"/>
+<node CREATED="1201425332000" ID="Freemind_Link_1554638514" MODIFIED="1201425335031" STYLE="bubble" TEXT="Code smells"/>
+</node>
+<node CREATED="1201425348109" ID="Freemind_Link_361579920" MODIFIED="1201425406843" STYLE="bubble" TEXT="Recognize user customizations in generated code"/>
+<node CREATED="1201425407281" FOLDED="true" ID="Freemind_Link_544812004" MODIFIED="1201425460843" STYLE="bubble" TEXT="Executable syntax definitions">
+<node CREATED="1201425461609" ID="Freemind_Link_1650689360" MODIFIED="1201425472406" STYLE="bubble" TEXT="Validation of sample sentences"/>
+<node CREATED="1201425472609" ID="Freemind_Link_1976071299" MODIFIED="1201425478921" STYLE="bubble" TEXT="Debugging of a grammar"/>
+</node>
+<node CREATED="1201425569437" FOLDED="true" ID="Freemind_Link_4369142" MODIFIED="1201446589082" STYLE="bubble" TEXT="Gramar restoration/mining">
+<node CREATED="1201425575218" ID="Freemind_Link_1397631753" MODIFIED="1201425589625" STYLE="bubble" TEXT="We have a lot of sentences but no grammar"/>
+<node CREATED="1201446560582" ID="Freemind_Link_1853334060" MODIFIED="1201446577332" TEXT="We have some semi-formal spec in the language reference"/>
+</node>
+</node>
+<node CREATED="1201433044819" FOLDED="true" ID="Freemind_Link_1678418158" MODIFIED="1201433049584" STYLE="bubble" TEXT="Abstract use cases">
+<node CREATED="1201433055146" FOLDED="true" ID="Freemind_Link_1235919105" MODIFIED="1201433077488" STYLE="bubble" TEXT="Source code models">
+<node CREATED="1201433088206" ID="Freemind_Link_947224230" MODIFIED="1201433091737" STYLE="bubble" TEXT="Annotation"/>
+<node CREATED="1201433091987" ID="Freemind_Link_10158386" MODIFIED="1201433099846" STYLE="bubble" TEXT="Scaffolding"/>
+<node CREATED="1201433103423" ID="Freemind_Link_168343031" MODIFIED="1201433105564" STYLE="bubble" TEXT="Markup"/>
+<node CREATED="1201433147091" ID="Freemind_Link_490604667" MODIFIED="1201433153185" STYLE="bubble" TEXT="Effectively exchange formats"/>
+</node>
+<node CREATED="1201433078473" FOLDED="true" ID="Freemind_Link_128153767" MODIFIED="1201433190603" STYLE="bubble" TEXT="Intermediate program representations">
+<node CREATED="1201433415583" ID="Freemind_Link_597835398" MODIFIED="1201433417380" STYLE="bubble" TEXT="PDG"/>
+<node CREATED="1201433417645" ID="Freemind_Link_482520944" MODIFIED="1201433419114" STYLE="bubble" TEXT="SSA"/>
+</node>
+<node CREATED="1201433446002" FOLDED="true" ID="Freemind_Link_836345119" MODIFIED="1201433452673" STYLE="bubble" TEXT="Domain-specific exchange formats">
+<node CREATED="1201433708434" ID="Freemind_Link_1133917332" MODIFIED="1201433711106" STYLE="bubble" TEXT="ATerm"/>
+<node CREATED="1201433711325" ID="Freemind_Link_723867061" MODIFIED="1201433725199" STYLE="bubble" TEXT="GXL"/>
+</node>
+<node CREATED="1201433762696" FOLDED="true" ID="Freemind_Link_675902057" MODIFIED="1201433767321" STYLE="bubble" TEXT="Interaction protocols">
+<node CREATED="1201433815396" ID="Freemind_Link_124204135" MODIFIED="1201433820676" STYLE="bubble" TEXT="Session types"/>
+</node>
+</node>
+<node CREATED="1201433889125" ID="Freemind_Link_139878631" MODIFIED="1201516333000" STYLE="bubble" TEXT="Language processing">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201433895906" ID="Freemind_Link_1210251971" MODIFIED="1201433899452" STYLE="bubble" TEXT="Debuggers"/>
+<node CREATED="1201433899718" ID="Freemind_Link_1878400378" MODIFIED="1201433912842" STYLE="bubble" TEXT="Program specializers"/>
+<node CREATED="1201433916435" ID="Freemind_Link_484269148" MODIFIED="1201433920248" STYLE="bubble" TEXT="Pre-processors"/>
+<node CREATED="1201433920529" ID="Freemind_Link_620509963" MODIFIED="1201436007278" STYLE="bubble" TEXT="Pretty printers"/>
+<node CREATED="1201436007778" ID="Freemind_Link_930952510" MODIFIED="1201436029089" STYLE="bubble" TEXT="Documentation generators"/>
+<node CREATED="1201458957341" ID="Freemind_Link_1314509086" MODIFIED="1201458963201" TEXT="Error handling rules"/>
+</node>
+<node CREATED="1201436120881" FOLDED="true" ID="Freemind_Link_197210472" MODIFIED="1201436126240" STYLE="bubble" TEXT="Broader meaning">
+<node CREATED="1201436126850" FOLDED="true" ID="Freemind_Link_1747544768" LINK="http://www.google.ru/url?sa=t&amp;ct=res&amp;cd=1&amp;url=http%3A%2F%2Fwiki.di.uminho.pt%2Ftwiki%2Fpub%2FResearch%2FVoda%2FWebHome%2FGBSs-Rebernak.pdf&amp;ei=InacR7veIZKAwgGwzM3fBw&amp;usg=AFQjCNEZwTUQbJfY6moH3kQfxvEeOYlifQ&amp;sig2=FrwBzbyssWLM7gKUXI2K9g" MODIFIED="1201436261124" STYLE="bubble" TEXT="Mernik et al 2004">
+<node CREATED="1201436176816" ID="Freemind_Link_1026165440" MODIFIED="1201436178753" TEXT="MERNIK, M., &#x2c7; CREPIN&#x2c7; SEK,M., KOSAR, T., REBERNAK, D., AND &#x2c7; ZUMER, V. 2004. Grammar-based systems: De&#xfb01;nition and examples. Informatica 28, 3 (Nov.), 245&#x2013;255."/>
+</node>
+</node>
+</node>
+<node CREATED="1201424492656" ID="_" MODIFIED="1201424544468" POSITION="left" STYLE="bubble" TEXT="Problems">
+<node CREATED="1201432862164" FOLDED="true" ID="Freemind_Link_770538297" MODIFIED="1201516245046" STYLE="bubble" TEXT="Enriched structural descriptions">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201432896036" ID="Freemind_Link_852907852" MODIFIED="1201432913019" STYLE="bubble" TEXT="Structure annotated by something else"/>
+</node>
+<node CREATED="1201447260515" ID="Freemind_Link_383716678" MODIFIED="1201447268640" TEXT="Support for grammar evolution"/>
+<node CREATED="1201447319686" ID="Freemind_Link_710864350" MODIFIED="1201516249031" TEXT="Delay commitment to specific technology">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1201447512123" FOLDED="true" ID="Freemind_Link_1347972350" MODIFIED="1201516252750" TEXT="Adaptation of general grammar to the requirements of the technology">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201447534169" ID="Freemind_Link_422960146" MODIFIED="1201447537982" TEXT="LR(k)"/>
+<node CREATED="1201447538279" ID="Freemind_Link_1191231538" MODIFIED="1201447541029" TEXT="LALR"/>
+<node CREATED="1201447541263" ID="Freemind_Link_1331384026" MODIFIED="1201447544794" TEXT="LL(*)"/>
+</node>
+<node CREATED="1201447769934" ID="Freemind_Link_977445982" MODIFIED="1201516259343" TEXT="Lack of modularity">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1201458839795" ID="Freemind_Link_779720596" MODIFIED="1201516262421" TEXT="Reuse">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1201424784890" ID="Freemind_Link_1747865382" MODIFIED="1201424792859" POSITION="left" STYLE="bubble" TEXT="Actions">
+<node CREATED="1201424793609" ID="Freemind_Link_1622106269" MODIFIED="1201424795296" STYLE="bubble" TEXT="Design"/>
+<node CREATED="1201424795484" ID="Freemind_Link_988043541" MODIFIED="1201516287406" STYLE="bubble" TEXT="Customization">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1201424808609" ID="Freemind_Link_516178789" MODIFIED="1201516287484" STYLE="bubble" TEXT="Implementation">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1201424812171" ID="Freemind_Link_131011793" MODIFIED="1201424826093" STYLE="bubble" TEXT="Testing"/>
+<node CREATED="1201424826296" ID="Freemind_Link_767314154" MODIFIED="1201424829328" STYLE="bubble" TEXT="Debugging"/>
+<node CREATED="1201424829656" ID="Freemind_Link_1585367715" MODIFIED="1201424834828" STYLE="bubble" TEXT="Versioning"/>
+<node CREATED="1201424835062" ID="Freemind_Link_495182861" MODIFIED="1201516287484" STYLE="bubble" TEXT="Transformation">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1201436434006" ID="Freemind_Link_673215217" MODIFIED="1201516347156" POSITION="right" TEXT="Meta-grammarware">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201436464910" ID="Freemind_Link_65406547" MODIFIED="1201516387000" TEXT="Fabula">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201436442177" ID="Freemind_Link_9136476" MODIFIED="1201436455536" TEXT="Take an [enriched] grammar"/>
+<node CREATED="1201436455770" ID="Freemind_Link_1260846053" MODIFIED="1201436463270" TEXT="Produce some program"/>
+</node>
+<node CREATED="1201436473738" FOLDED="true" ID="Freemind_Link_1566686825" MODIFIED="1201436479285" TEXT="Functions">
+<node CREATED="1201436480566" ID="Freemind_Link_1996472735" MODIFIED="1201436498502" TEXT="Software transformation"/>
+<node CREATED="1201436498721" ID="Freemind_Link_897955486" MODIFIED="1201436503627" TEXT="Program analysis"/>
+<node CREATED="1201436503830" ID="Freemind_Link_395455651" MODIFIED="1201436514486" TEXT="Language processing"/>
+<node CREATED="1201436514705" ID="Freemind_Link_849580349" MODIFIED="1201436518376" TEXT="Program generation"/>
+<node CREATED="1201436587732" ID="Freemind_Link_1853901229" MODIFIED="1201436602028" TEXT="Immediate derivation of usable software"/>
+</node>
+<node CREATED="1201436641448" ID="Freemind_Link_1415191893" MODIFIED="1201516363546" TEXT="Enrichments">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1201436644917" FOLDED="true" ID="Freemind_Link_922973649" MODIFIED="1201436649417" TEXT="Parser specifications">
+<node CREATED="1201436650089" ID="Freemind_Link_1420183663" MODIFIED="1201436652010" TEXT="YACC"/>
+<node CREATED="1201436652245" ID="Freemind_Link_860863986" MODIFIED="1201436654104" TEXT="ANTLR"/>
+<node CREATED="1201436654323" ID="Freemind_Link_1164516923" MODIFIED="1201436658119" TEXT="SableCC"/>
+</node>
+<node CREATED="1201436659307" FOLDED="true" ID="Freemind_Link_1179853955" MODIFIED="1201436692524" TEXT="Test-sets specifications">
+<node CREATED="1201436693212" ID="Freemind_Link_156812068" LINK="http://portal.acm.org/citation.cfm?id=624965" MODIFIED="1201436906422" TEXT="DGL"/>
+</node>
+<node CREATED="1201436928984" ID="Freemind_Link_273101978" MODIFIED="1201436935734" TEXT="Pretty printing specifications"/>
+<node CREATED="1201436935984" ID="Freemind_Link_749183445" MODIFIED="1201436955077" TEXT="Serializable object models"/>
+</node>
+</node>
+</node>
+</map>
