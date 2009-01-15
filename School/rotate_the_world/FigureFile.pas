0e98ec3c8d977b992e0f57758c1be986839705ae
abreslav <abreslav@f8d81c2c-b11e-0410-9df4-1f401b7bbd07>
2009-01-15 07:06:31 +0000

    nothing

--- School/rotate_the_world/FigureFile.pas
+++ School/rotate_the_world/FigureFile.pas
@@ -60,17 +60,17 @@ implementation
 
 procedure Reset(var fFile : File);
 begin
-
+  System.Reset(fFile, 1);
 end;
 
 procedure CloseFile(var fFile : File);
 begin
-
+  System.CloseFile(fFile);
 end;
 
 function GetNextFigureType(var fFile : File) : TFigureType;
 begin
-  Result := ftError;
+  BlockRead(fFile, Result, sizeOf(Result));
 end;
 
 function HasMoreFigures(var fFile : File) : Boolean;
